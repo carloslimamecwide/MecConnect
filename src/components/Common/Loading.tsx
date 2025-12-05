f2143bafccadb12d1e65ca356a1789e71e73ac9c
carloslimamecwide <carlos.lima@mecwide.com>
2025-12-05 15:18:54 +0000

    feat: Atualizar estilos do componente de carregamento para melhorar a aparência e a legibilidade

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 
-const Loading = ({message,size}: {message: string, size: "small" | "large"}) => {
+const Loading = ({ message, size }: { message: string; size: "small" | "large" }) => {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-100">
-      <ActivityIndicator size={size} color="#3b82f6" />
-      <Text className="mt-4 text-slate-600">{message}</Text>
+    <View className="flex-1 justify-center items-center bg-gray-900">
+      <ActivityIndicator size={size} color="#FFF" />
+      <Text className="mt-4 text-slate-100">{message}</Text>
     </View>
   );
 };
