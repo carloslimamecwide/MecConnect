452c1121905a8891a404aa1163dd243a49c8c5c7
carloslimamecwide <carlos.lima@mecwide.com>
2026-01-22 13:51:14 +0000

    feat: adicionar suporte para cor personalizada no componente Loading

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ActivityIndicator, Text, View } from "react-native";
 
-const Loading = ({ message, size }: { message: string; size: "small" | "large" }) => {
+const Loading = ({ message, size, color }: { message: string; size: "small" | "large"; color?: string }) => {
   return (
-    <View className="flex-1 justify-center items-center bg-gray-900">
-      <ActivityIndicator size={size} color="#FFF" />
+    <View className="flex-1 justify-center items-center">
+      <ActivityIndicator size={size} color={color || "#60a5fa"} />
       <Text className="mt-4 text-slate-100">{message}</Text>
     </View>
   );
